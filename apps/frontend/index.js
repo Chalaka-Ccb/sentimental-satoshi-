0f1fd3146e6661872ef3f3ef3103d0f132ad73be
Chalaka-Ccb <COHNDSE242F-074@student.nibm.lk>
2026-04-16 13:58:30 +0530

    Set up enviroment to move on

---
+++ apps/frontend/index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Frontend placeholder');
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`Frontend listening on ${port}`));
